8c7b8c2d059d5dc80d0af1f590558aa5facbdae1
uPiscium <upiscium@gmail.com>
2024-11-13 10:38:25 +0900

    Add OpenAL helper functions.

--- includes/TerreateAudio.hpp
+++ includes/TerreateAudio.hpp
@@ -1,4 +1,14 @@
 #ifndef __TERREATEAUDIO_HPP__
 #define __TERREATEAUDIO_HPP__
 
+#include "ALHelper.hpp"
+#include "defines.hpp"
+
+namespace TerreateAudio {
+using namespace TerreateAudio::Defines;
+
+inline Bool Initialize() noexcept { return ALHelper::Initialize(); }
+inline void Terminate() noexcept { ALHelper::Terminate(); }
+} // namespace TerreateAudio
+
 #endif // __TERREATEAUDIO_HPP__
